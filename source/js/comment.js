9f53af624a62e80c78bb0f4619206dd740f496eb
mac <mac@macs-Mac-mini.local>
2020-12-17 20:43:46 +0800

    主题样式

---
+++ source/js/comment.js
@@ -0,0 +1,8 @@
+console.log('%c 欢迎来到我的博客！',
+'background-color: #49b1f5; color: #fff; padding: 8px;font-size:14px;')
+
+console.log('%c 既然来啦，就留个言吧',
+'background-color: #49b1f5; color: #fff; padding: 8px;font-size:14px;',
+'https://shuxhan.com/message',)
+
+
